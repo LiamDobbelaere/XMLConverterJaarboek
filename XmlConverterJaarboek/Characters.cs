502afba614dc665763c48689f203d9217eb73072
Tom Dobbelaere <tom.dobbelaere@student.howest.be>
2018-05-15 16:03:31 +0200

    Another fix

--- XmlConverterJaarboek/Characters.cs
+++ XmlConverterJaarboek/Characters.cs
@@ -9,6 +9,6 @@ namespace XmlConverterJaarboek
     {
         public static string PARAGRAPH_SEP = "\u2029";
         public static string FIXED_SPACE = " ";
-        public static string NOBREAK_HYPHEN = "\u2011";
+        public static string NOBREAK_HYPHEN = "-";
     }
 }
